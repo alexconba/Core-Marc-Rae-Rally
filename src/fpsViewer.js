2db8d0d9ca313ae6c1a71d1538c22f66c123c0f4
alexconba <alexcontrerasbardina@gmail.com>
2022-05-12 16:46:58 +0200

    bones-complet

---
+++ src/fpsViewer.js
@@ -0,0 +1,16 @@
+class FPSViewer {
+  constructor(position) {
+    this.position = position;
+  }
+
+  keyboard_event() {}
+
+  update() {}
+
+  draw(delta, ctx) {
+    const fps = (1 / delta).toFixed(2);
+    ctx.font = "50px Arial";
+    ctx.fillStyle = "black";
+    ctx.fillText(`FPS: ${fps}`, this.position.x, this.position.y);
+  }
+}
---
+++ src/server.js
@@ -0,0 +1,31 @@
+const pino = require("pino");
+const path = require("path");
+const fastify = require("fastify")({
+  logger: pino({
+    transport: {
+      target: "pino-pretty",
+    },
+  }),
+});
+
+fastify.register(require("@fastify/static"), {
+  root: path.join(__dirname, "../public"),
+  prefix: "/public/",
+});
+
+fastify.register(require("point-of-view"), {
+  engine: {
+    handlebars: require("handlebars"),
+  },
+  root: path.join(__dirname, "../views"),
+  layout: "/templates/main.hbs",
+});
+
+fastify.register(require("@fastify/formbody"));
+
+fastify.register(require("./routes/main.router"));
+
+const port = 3000;
+fastify.listen(port, () => {
+  fastify.log.info(`Listening at http://localhost:${port}`);
+});
